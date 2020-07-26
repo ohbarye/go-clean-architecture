fae4b0d8064f67ac80152e66b77e0194a9e2cdb0
ohbarye <over.rye@gmail.com>
2020-07-26 15:55:58 +0900

    Set up docker compose

---
+++ README.md
@@ -0,0 +1,14 @@
+# Go Clean Architecture
+
+## Development
+
+```shell
+$ docker-compose up
+```
+
+### Connect to MySQL
+
+```shell
+$ mysql -u root -h localhost -P 3306 --protocol=tcp
+```
+
---
+++ mysql/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
