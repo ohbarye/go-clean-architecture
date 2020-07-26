b20508ca37e1423fa4bbb112195cde2932d4682f
ohbarye <over.rye@gmail.com>
2020-07-26 17:30:25 +0900

    Add logger middleware

--- src/infrastructure/router.go
+++ src/infrastructure/router.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"go-clean-architecture/interfaces/controllers"
 )
 
@@ -9,6 +10,8 @@ func Run()  {
 	e := echo.New()
 
 	userController := controllers.NewUserController(NewSqlHandler())
+
+	e.Use(middleware.Logger())
 
 	e.POST("/users", func(c echo.Context) error { return userController.Create(c) })
 	e.GET("/users", func(c echo.Context) error { return userController.Index(c) })
