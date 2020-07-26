80fa2bca199c6c47827adb9bddb440d9871d6b7e
ohbarye <over.rye@gmail.com>
2020-07-26 16:01:42 +0900

    Use echo in server.go

--- src/server.go
+++ src/server.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
+	})
+
+	e.Logger.Fatal(e.Start(":1313"))
 }
